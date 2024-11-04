97a2a535c224b23b94374ffaaebeb648573d0584
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-11-04 13:27:39 +0100

    Adding outbox to watcher

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,6 +27,7 @@ module.exports = {
      {
       name   : "exp-outbox",
       script : "cd ../mastodon-bot ; LOG4JS=info npx ldn-inbox-server handler @outbox -hn @handler/notification_handler/multi.js" ,
+      watch  : [ "../mastodon-bot/outbox" ] ,
       watch_delay : 10000 ,
       env: {
         DEMO_MODE: 'NO_TOOTS',
