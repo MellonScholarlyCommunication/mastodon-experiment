e9084f678bf4bb8bac034bdbce12cb3e02b1a319
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-11-06 12:04:53 +0100

    Spacing

--- bin/stats.sh
+++ bin/stats.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 echo "[Number of processed toots URLs]"
-echo
 
 npx eventlog-server list -qp "generator.id=https://www.npmjs.com/package/mastodon-cli" | wc -l
 
-echo "[Number of failed toots URLs]"
 echo
+echo "[Number of failed toots URLs]"
 
 npx eventlog-server list -qp "type=Reject" | wc -l
 
-echo "[Failed toot URLs + reason]"
 echo
+echo "[Failed toot URLs + reason]"
 
 npx eventlog-server export -qp "type=Reject" | jq -r '.data.object.object.id + " => " + .data.summary'
 
-echo "[Testing failed toots]"
 echo
+echo "[Testing failed toots]"
 
 npx eventlog-server export -qp "type=Reject" | jq -r '.data.object.object.id' | xargs -n 1 ./bin/link_test.js
