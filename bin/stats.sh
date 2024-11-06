71f98f2330e912b735bbf2501973131219f2c244
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-11-06 11:00:26 +0100

    Fixing stats for yarn 4

--- bin/stats.sh
+++ bin/stats.sh
@@ -2,12 +2,12 @@
 
 echo "Number of processed toots URLs:"
 
-yarn -s event-admin list -qp "generator.id=https://www.npmjs.com/package/mastodon-cli" | wc -l
+npx eventlog-server list -qp "generator.id=https://www.npmjs.com/package/mastodon-cli" | wc -l
 
 echo "Number of failed toots URLs:"
 
-yarn -s event-admin list -qp "type=Reject" | wc -l
+npx eventlog-server list -qp "type=Reject" | wc -l
 
 echo "Failed toot URLs:"
 
-yarn -s event-admin export -qp "type=Reject" | jq ".data.object.object.id"
+npx eventlog-server event-admin export -qp "type=Reject" | jq ".data.object.object.id"
