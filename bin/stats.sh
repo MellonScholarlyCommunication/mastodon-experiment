ddb73c6ae8defc5d683b387fb2954e8311ac4a25
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-11-14 08:50:11 +0100

    Adding url stats

--- bin/stats.sh
+++ bin/stats.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-echo "[Number of processed toots URLs]"
+echo "[Number of processed toots]"
 
 npx eventlog-server list -qp "generator.id=https://www.npmjs.com/package/mastodon-cli" | wc -l
+
+echo "[Number of processed toot URLs]"
+
+TMP=$(mktemp)
+
+npx eventlog-server export -qp "generator.id=https://www.npmjs.com/package/mastodon-cli" | jq -r ".data.object.url | select (. != null) | .[].href" > ${TMP}
+
+cat ${TMP} | wc -l
+
+URL_UNQ=$(cat ${TMP} | sort -u | wc -l)
+echo "${URL_UNQ} (unique)"
+
+rm ${TMP}
 
 echo
 echo "[Number of failed toots URLs]"
