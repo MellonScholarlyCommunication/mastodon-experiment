f09c994234d61322cb87cc1a6c7c440f5bf3abff
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-11-18 10:03:59 +0100

    Fixing adding stats to end experiment

--- bin/end_experiment.sh
+++ bin/end_experiment.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-DATE=$(date -%Y%d%m)
+if [ "$1" != "" ]; then
+    DATE=$1
+else
+    DATE=$(date -%Y%d%m)
+fi
 
 npx eventlog-server export > ./data/experiment-export-${DATE}.json
 
 zip -r ./data/experiment-results-${DATE}.zip ../metadata-server/public/result
+
+./bin/stats.sh > ./data/experiment-stats-${DATE}.txt
