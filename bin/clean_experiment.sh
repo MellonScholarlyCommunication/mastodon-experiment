ecc81fcc38f7866cb5c2f90c492ef88cff4ba2c2
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-11-18 10:30:46 +0100

    Adding verbose to bash scripts

--- bin/clean_experiment.sh
+++ bin/clean_experiment.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "Cleaning accepted..."
 yarn real-clean
 
+echo "Cleaning cache..."
 npx eventlog-server remove-all
 
+echo "Cleaning wiki..."
 ./bin/clean_wiki.sh
--- bin/start_experiment.sh
+++ bin/start_experiment.sh
@@ -2,15 +2,21 @@
 
 DATE=$(date -%Y%d%m)
 
-./bin/clean_experiment.sh
-
+echo "Stopping ecosystem..."
 pm2 stop ecosystem.config.js
+
+echo "Starting ecosystem..."
 pm2 start ecosystem.config.js
 
+echo "Fetching toots for researchers..."
 yarn fetch-all
 
 if [ ! -d data ];
     mkdir data
 fi
 
+echo "Archiving accepted toots in data/experiment-accepted-${DATE}.zip ..."
 zip -r data/experiment-accepted-${DATE}.zip ./accepted
+
+echo "Now you can run: yarn run-experiment or yarn run-one"
+echo "Doei!"
