075f713e933568b6812f9f1af03881d420a742d3
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2024-11-22 11:52:12 +0100

    Adding cleaning of error directories

--- bin/clean_experiment.sh
+++ bin/clean_experiment.sh
@@ -11,3 +11,9 @@ npx eventlog-server --name claims remove-all
 
 echo "Cleaning wiki..."
 ./bin/clean_wiki.sh
+
+echo "Cleaning ../mastodon-bot/error"
+rm -f ../mastodon-bot/error/*
+
+echo "Cleaning ../metadata-server/error"
+rm -f ../metadata-server/error/*
